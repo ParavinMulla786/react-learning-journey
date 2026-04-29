224b0b2c5c73647132de8fe475ff8532f5b25264
paravinmullananotechsoftwares <paravinmulla2004@gmail.com>
2026-04-30 00:33:33 +0530

    added tribute page components

--- tribute-page/src/App.jsx
+++ tribute-page/src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-import './App.css'
-import Header from './components/Header'
-import './components/Header.css'
-import Home from './components/Home'
-import './components/Home.css'
+import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Stats from "./components/Stats";
+import About from "./components/About";
+import Highlights from "./components/Highlights";
+import Quote from "./components/Quote";
+import Footer from "./components/Footer";
+import Career from "./components/Career";
+import Achievements from "./components/Achievements";
+import Gallery from "./components/Gallery";
 
 function App() {
- return(
-    <div className='container'>
-        <div className='header'>
-  <Header/>
-  </div>
-  <div className='home'>
-    <Home/>
-  </div>
-  </div>
- )}
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Stats />
+      <About />
+      <Career />
+      <Achievements />
+      <Highlights />
+      <Gallery />
+      <Quote />
+      <Footer />
+    </>
+  );
+}
 
 export default App
